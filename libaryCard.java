01883be3e8096850ba93d8eb3d00af3c5d79c963
Robert Stebbins <48532176+robertstebbins@users.noreply.github.com>
2022-07-26 12:19:22 -0500

    Update libaryCard.java

--- libaryCard.java
+++ libaryCard.java
@@ -1,67 +1,76 @@
 class libaryCard{
 
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int phoneNumber;
+    private boolean isChild;
+    private double fine;
+    private int cardNumber;
   
-
-  private String firstName;
-  private String lastName;
-  private String address;
-  private int phoneNumber;
-  private boolean isChild;
-  private double fine;
-  private int cardNumber;
-
-
-  public String getName(){
-      return firstName;
-    }
-
-  public String getLastName(){
-      return lastName;
-  }
-
-  public String  getAddress(){
-      return address;
-    }
-
-  public int  getID(){
-      return carNumber;
-    }
-
-  public int  getPhoneNumber(){
-      return phoneNumber;
+    public libaryCard(String firstName, String lastName, String address, int phoneNumber, int age)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        if(age <= 12)
+            isChild = true;
+        else
+            isChild = false;
     }
   
-  public int  getFine(){
-      return fine;
+    public String getName(){
+        return firstName;
+      }
+  
+    public String getLastName(){
+        return lastName;
     }
-
-  public boolean isChild(){
-      this.isChild = false;
+  
+    public String  getAddress(){
+        return address;
+      }
+  
+    public int  getID(){
+        return cardNumber;
+      }
+  
+    public int  getPhoneNumber(){
+        return phoneNumber;
+      }
+  
+    public double  getFine(){
+        return fine;
+      }
+  
+    public boolean isChild(){
+        return isChild;
+      }
+  
+  
+    public void setName(String newName){
+        this.firstName = newName;
     }
-
-
-  public void setName(String newName){
-      this.firstName = newName;
-  }
-  public void setLastName(String newLastName){
-      this.lastName = newLastName;
-  }
-  public void setAddress(String newAddress){
-    this.address = newAddress;
-  }
-
-  public void setPhoneNumber(int newPhoneNumber){
-      this.phoneNumber = newPhoneNumber;
+    public void setLastName(String newLastName){
+        this.lastName = newLastName;
     }
-
-  public void setFine(int newFine){
-      this.fine = newFine;
+    public void setAddress(String newAddress){
+      this.address = newAddress;
+    }
+  
+    public void setPhoneNumber(int newPhoneNumber){
+        this.phoneNumber = newPhoneNumber;
+      }
+  
+    public void setFine(int newFine){
+        this.fine = newFine;
+    }
+  
+    public void addFine(int updateFine){
+        this.fine += updateFine;
+    }
+  
+  
   }
-
-  public void addFine(int updateFine){
-      this.fine += updateFine;
-  }
-
-
-
-}
