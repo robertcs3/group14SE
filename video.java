c88733061ab1eb2a2852af19a6ccb46f5dd9539e
An Ngo <ngothanhan1994@gmail.com>
2022-07-26 16:29:17 -0500

    implement book, audio, and video objects

---
+++ video.java
@@ -0,0 +1,75 @@
+public class video implements  CheckOutAble{
+    String name;
+    double itemValue = 0;
+    int durationLimit = 14;//days
+    int itemID = 0;
+    int dateCheckout = 0;
+    int copies = 1;
+    boolean isRenew = false;
+
+    //Default constructor
+    public video(int itemID, int value, String name)
+    {
+        //Set video's values
+        this.itemID = itemID;
+        itemValue = value;
+        this.name = name;
+    }
+
+    @Override
+    public void setRenewStatus(boolean status) {
+        isRenew = status;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getDateCheckout() {
+        return dateCheckout;
+    }
+
+    @Override
+    public int getDurationLimit() {
+        return durationLimit;
+    }
+
+    @Override
+    public int getID() {
+        return itemID;
+    }
+
+    @Override
+    public void setID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    @Override
+    public int getCopies() {
+        return copies;
+    }
+
+    @Override
+    public void addCopies(int amount) {
+        copies += amount;
+    }
+
+    @Override
+    public void decreaseCopy(int amount) {
+        if(amount >= copies)
+        {
+            copies = 0;
+        }
+        else
+        {
+            copies-=amount;
+        }
+    }
+}
