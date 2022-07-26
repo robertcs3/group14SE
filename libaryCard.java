aab3a0983863b8f741d6ef7ce9bd21841e2387bb
Nestor Jimenez <jnestor411@gmail.com>
2022-07-26 12:03:26 -0500

    Created attributes/getters-setters for libraryCard

---
+++ libaryCard.java
@@ -0,0 +1,65 @@
+class libaryCard{
+
+
+  private String firstName;
+  private String lastName;
+  private String address;
+  private int phoneNumber;
+  private boolean isChild;
+  private double fine;
+  private int cardNumber;
+
+
+  public String getName(){
+      return firstName;
+    }
+
+  public String getLastName(){
+      return lastName;
+  }
+
+  public String  getAddress(){
+      return address;
+    }
+
+  public int  getID(){
+      return carNumber;
+    }
+
+  public int  getPhoneNumber(){
+      return phoneNumber;
+    }
+
+  public int  getFine(){
+      return fine;
+    }
+
+  public boolean isChild(){
+      this.isChild = false;
+    }
+
+
+  public void setName(String newName){
+      this.firstName = newName;
+  }
+  public void setLastName(String newLastName){
+      this.lastName = newLastName;
+  }
+  public void setAddress(String newAddress){
+    this.address = newAddress;
+  }
+
+  public void setPhoneNumber(int newPhoneNumber){
+      this.phoneNumber = newPhoneNumber;
+    }
+
+  public void setFine(int newFine){
+      this.fine = newFine;
+  }
+
+  public void addFine(int updateFine){
+      this.fine += updateFine;
+  }
+
+
+}
