d3e6eb82d2b2f787ded43b7a723e02da9d800815
Robert Stebbins <48532176+robertstebbins@users.noreply.github.com>
2022-07-26 17:16:06 -0500

    Set up functions in checkoutTracker

--- checkoutTracker.java
+++ checkoutTracker.java
@@ -1,3 +1,36 @@
-class checkoutTracker{
+import java.util.HashMap;
+
+class checkoutTracker extends librarySystem{
+
+    public checkoutTracker(String firstName, String lastName, String address, int phoneNumber, int age) {
+        super(firstName, lastName, address, phoneNumber, age);
+        //TODO Auto-generated constructor stub
+    }
+
+    HashMap<Integer, String> checkoutLog = new HashMap<Integer, String>();
+
+    public void checkOutItem(int itemID, int userID){
+
+    }
+
+    public boolean renewItem(int itemID, int userID){
+        //placeholder
+        return true;
+    }
+
+    public boolean checkOutstandingRequest(int itemID, int userID){
+        //placeholder
+        return true;
+    }
+
+    public void returnItem(int itemID, int userID){
+
+    }
+
+    public double outstandingFine(int userID){
+        //placeholder
+        return userID;
+
+    }
   
 }
