fbcb32a2f56fabf23325712f13fe47d39d721113
An Ngo <ngothanhan1994@gmail.com>
2022-07-25 20:47:38 -0500

    testing

---
+++ Library.java
@@ -0,0 +1,8 @@
+public class Library
+{
+    public static void main(String args[])
+    {
+        libaryCard card = new libaryCard("An", "Ngo", "509 Granger rd, Texas 78626");
+        card.toString();
+    }
+}
--- libaryCard.java
+++ libaryCard.java
@@ -1,4 +1,20 @@
 class libaryCard
 {
-  
+    private String firstName, lastName, address;
+    private int idNumber;
+  public libaryCard(String fName, lName, address)
+  {
+      firstName = fName;
+      lastName = lName;
+      idNumber = Math.Random() * 10000000;
+      this.address = address;
+  }
+
+  public String toString()
+  {
+      System.out.println(firstName);
+      System.out.println(lastName);
+      System.out.println(address);
+      System.out.println(idNumber);
+  }
 }
